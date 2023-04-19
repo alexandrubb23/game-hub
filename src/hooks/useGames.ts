b933e2d5a1572550757540b61ef510b094a67d6e
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-04-19 09:48:49 +0300

    Show loading skeletons.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,18 +25,22 @@ interface GamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setIsLoading(true);
     apiClient
       .get<GamesResponse>('/games', { signal: controller.signal })
       .then(response => {
         setGames(response.data.results);
+        setIsLoading(false);
       })
       .catch(error => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setIsLoading(false);
       });
 
     return () => {
@@ -44,7 +48,7 @@ const useGames = () => {
     };
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
