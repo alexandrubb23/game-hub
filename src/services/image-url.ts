e3b6849dc44a370437a318b31da806b4693e2bc2
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-04-19 09:34:35 +0300

    Get optimized images.

---
+++ src/services/image-url.ts
@@ -0,0 +1,8 @@
+const getCroppedImageUrl = (url: string, width = 600, height = 400) => {
+  const target = 'media/';
+  const index = url.indexOf(target) + target.length;
+
+  return url.slice(0, index) + `crop/${width}/${height}/` + url.slice(index);
+};
+
+export default getCroppedImageUrl;
