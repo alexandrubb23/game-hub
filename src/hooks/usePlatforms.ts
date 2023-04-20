87af6d0c4a42bd8764aeaa4b783d0fb2b0971fec
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-04-20 10:50:21 +0300

    Build platform selector.

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import useData from './useData';
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+const usePlatforms = () => useData<Platform>('/xplatforms/lists/parents');
+
+export default usePlatforms;
