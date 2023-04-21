6d552ecf105f1fb51be0758a7cac8de86c544fa3
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-04-21 17:16:40 +0300

    Implement searching.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,31 @@
+import { useRef } from 'react';
 import { Input, InputGroup, InputLeftAddon } from '@chakra-ui/react';
 import { BsSearch } from 'react-icons/bs';
 
-const SearchInput = () => {
+interface SearchInputProps {
+  onSearch: (search: string) => void;
+}
+
+const SearchInput = ({ onSearch }: SearchInputProps) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <InputGroup>
-      <InputLeftAddon children={<BsSearch />} />
-      <Input borderRadius={20} placeholder='Search games...' variant='filled' />
-    </InputGroup>
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftAddon children={<BsSearch />} />
+        <Input
+          borderRadius={20}
+          placeholder='Search games...'
+          variant='filled'
+          ref={ref}
+        />
+      </InputGroup>
+    </form>
   );
 };
 
