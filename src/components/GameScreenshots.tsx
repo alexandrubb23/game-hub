b302483c022c847371f676e375b350d027d74bb1
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-06-28 16:42:56 +0300

    Build game screenshots.

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,30 @@
+import {
+  Grid,
+  GridItem,
+  Image,
+  SimpleGrid,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import useScreenshots from '../hooks/useScreenshots';
+
+interface GameScreenshotsProps {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: GameScreenshotsProps) => {
+  const { data, isLoading, error } = useScreenshots(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      {data?.results.map(file => (
+        <Image key={file.id} src={file.image} alt='Game screenshot' />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
