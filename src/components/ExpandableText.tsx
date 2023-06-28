b6167cbf94aabc1d360e62c14c20f7cb423e1d17
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-06-28 20:04:24 +0300

    Refactor- expandable text.

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -6,14 +6,17 @@ interface ExpandableTextProps {
   limit?: number;
 }
 
-const ExpandableText = ({ children, limit = 300 }: ExpandableTextProps) => {
+const ExpandableText = ({ children, limit = 500 }: ExpandableTextProps) => {
   const [expanded, setExpanded] = useState(true);
 
   if (children.length <= limit) return <Text>{children}</Text>;
 
+  // isTruncated could be used here, but it's not reliable
+  const text = expanded ? children.slice(0, limit) + '...' : children;
+
   return (
     <>
-      <Text isTruncated={expanded}>{children}</Text>
+      <Text>{text}</Text>
       <Button
         colorScheme='yellow'
         fontWeight='bold'
