23c5e612d0f2ec30987eee5b209a62dc24989fe7
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-06-30 17:12:11 +0300

    Add clear search button.

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -4,13 +4,16 @@ import { useGenre, usePlatform } from '../hooks';
 import useGameQueryStore from '../store';
 
 const GameHeading = () => {
+  const searchText = useGameQueryStore(s => s.gameQuery.searchText);
   const genreId = useGameQueryStore(s => s.gameQuery.genreId);
   const genre = useGenre(genreId);
 
   const platformId = useGameQueryStore(s => s.gameQuery.platformId);
   const platform = usePlatform(platformId);
 
-  const heading = `${platform?.name || ''} ${genre?.name || ''} Games`;
+  const heading = `${searchText ? `Search after "${searchText}" in` : ''} ${
+    platform?.name || ''
+  } ${genre?.name || ''} Games`;
 
   return (
     <Heading as='h1' marginY={5} fontSize='5xl'>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,13 +1,16 @@
-import { useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
 import {
-  FormControl,
+  Button,
+  IconButton,
   Input,
   InputGroup,
   InputLeftAddon,
+  InputRightElement,
 } from '@chakra-ui/react';
-import { BsSearch } from 'react-icons/bs';
 import { debounce } from 'lodash';
+import { useRef } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+import { MdClear } from 'react-icons/md';
 
 import useGameQueryStore from '../store';
 
@@ -16,6 +19,7 @@ interface SearchInputProps {
 }
 
 const SearchInput = ({ wait = 500 }: SearchInputProps) => {
+  const searchInputRef = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore(s => s.setSearchText);
   const navigate = useNavigate();
 
@@ -27,18 +31,31 @@ const SearchInput = ({ wait = 500 }: SearchInputProps) => {
     wait
   );
 
+  const clearSearch = () => {
+    setSearchText('');
+    if (searchInputRef.current) searchInputRef.current.value = '';
+  };
+
   return (
-    <FormControl>
-      <InputGroup>
-        <InputLeftAddon children={<BsSearch />} />
-        <Input
-          borderRadius={20}
-          placeholder='Search games...'
-          variant='filled'
-          onChange={handleSearch}
+    <InputGroup>
+      <InputLeftAddon children={<BsSearch />} />
+      <Input
+        borderRadius={20}
+        placeholder='Search games...'
+        variant='filled'
+        onChange={handleSearch}
+        ref={searchInputRef}
+      />
+      <InputRightElement width='4.5rem'>
+        <IconButton
+          aria-label='Clear search'
+          h='1.75rem'
+          icon={<MdClear />}
+          onClick={() => clearSearch()}
+          size='sm'
         />
-      </InputGroup>
-    </FormControl>
+      </InputRightElement>
+    </InputGroup>
   );
 };
 
