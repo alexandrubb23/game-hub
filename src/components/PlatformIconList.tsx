1e2987df319ba72147f8c2dca639779e19f5a1cc
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-06-30 17:21:54 +0300

    Fix- missing platforms.

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -43,7 +43,7 @@ const PlatformIconList = ({ platforms }: PlatformIconListProps) => {
 
   return (
     <HStack marginY={1}>
-      {platforms.map(platform => (
+      {platforms?.map(platform => (
         <Icon as={iconMap[platform.slug]} color='gray.500' key={platform.id} />
       ))}
     </HStack>
